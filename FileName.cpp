4f8fa1614d4c63a1808c0afa552625907b143ad4
N <140025017+NaturalClown@users.noreply.github.com>
2025-12-25 13:40:11 +0700

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -5,21 +5,17 @@
 #include <fstream>
 #include <omp.h>
 
-// Отключаем макросы min/max из Windows.h
 #define NOMINMAX
 #include <windows.h>
 
-// Параметры из лабораторной работы
 const double ALPHA = 1e5;
 const double EPS = 1e-8;
 const int MAX_ITER = 100000;
 
-// Область [-1, 1]^3
 const double X_MIN = -1.0, X_MAX = 1.0;
 const double Y_MIN = -1.0, Y_MAX = 1.0;
 const double Z_MIN = -1.0, Z_MAX = 1.0;
 
-// Точное решение и правая часть
 double exact_solution(double x, double y, double z) {
     return x * x + y * y + z * z;
 }
@@ -28,7 +24,6 @@ double right_part(double x, double y, double z) {
     return 6.0 - ALPHA * exact_solution(x, y, z);
 }
 
-// Своя функция max чтобы избежать конфликта
 double my_max(double a, double b) {
     return (a > b) ? a : b;
 }
@@ -36,12 +31,11 @@ double my_max(double a, double b) {
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     int mpi_prof_enabled = 0;
-    MPI_Pcontrol(1); // Включить профилирование
+    MPI_Pcontrol(1); // Попытка профилирования
     int mpi_rank, mpi_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
-    // Параметры OpenMP
     int omp_threads = 1;
     if (argc >= 5) {
         omp_threads = atoi(argv[4]);
@@ -49,7 +43,7 @@ int main(int argc, char** argv) {
     omp_set_num_threads(omp_threads);
 
     // Размеры сетки
-    int Nx = 64, Ny = 64, Nz = 64;  // Уменьшил для быстрого теста
+    int Nx = 64, Ny = 64, Nz = 64; 
     if (argc >= 4) {
         Nx = atoi(argv[1]);
         Ny = atoi(argv[2]);
@@ -67,7 +61,6 @@ int main(int argc, char** argv) {
         std::cout << "========================================" << std::endl;
     }
 
-    // Шаги сетки
     double hx = (X_MAX - X_MIN) / (Nx - 1);
     double hy = (Y_MAX - Y_MIN) / (Ny - 1);
     double hz = (Z_MAX - Z_MIN) / (Nz - 1);
@@ -77,7 +70,6 @@ int main(int argc, char** argv) {
     double hz2 = hz * hz;
     double denominator = 2.0 / hx2 + 2.0 / hy2 + 2.0 / hz2 + ALPHA;
 
-    // ========== MPI ДЕКОМПОЗИЦИЯ ПО Z ==========
     int chunk_size = Nz / mpi_size;
     int remainder = Nz % mpi_size;
 
@@ -93,7 +85,6 @@ int main(int argc, char** argv) {
     }
     z_end = z_start + local_nz - 1;
 
-    // Добавляем ghost layers
     int total_layers = local_nz + 2;
 
     // Выделяем память
@@ -105,7 +96,6 @@ int main(int argc, char** argv) {
         return (i * Ny + j) * total_layers + layer;
         };
 
-    // ========== ИНИЦИАЛИЗАЦИЯ ==========
     double init_start = MPI_Wtime();
 
 #pragma omp parallel for
@@ -133,12 +123,10 @@ int main(int argc, char** argv) {
 
     double init_time = MPI_Wtime() - init_start;
 
-    // ========== ИТЕРАЦИИ ЯКОБИ ==========
     double compute_start = MPI_Wtime();
     int iter = 0;
     double global_diff = 1.0;
 
-    // Буферы для обмена
     std::vector<double> send_down(Nx * Ny);
     std::vector<double> send_up(Nx * Ny);
     std::vector<double> recv_down(Nx * Ny);
@@ -149,10 +137,8 @@ int main(int argc, char** argv) {
     while (global_diff > EPS && iter < MAX_ITER) {
         double local_diff = 0.0;
 
-        // ---- MPI: АСИНХРОННЫЙ ОБМЕН ГРАНИЦАМИ ----
         int req_count = 0;
 
-        // Отправляем нижнюю границу
         if (mpi_rank > 0) {
 #pragma omp parallel for
             for (int i = 0; i < Nx; i++) {
@@ -168,7 +154,6 @@ int main(int argc, char** argv) {
                 MPI_COMM_WORLD, &requests[req_count++]);
         }
 
-        // Отправляем верхнюю границу
         if (mpi_rank < mpi_size - 1) {
 #pragma omp parallel for
             for (int i = 0; i < Nx; i++) {
@@ -184,8 +169,6 @@ int main(int argc, char** argv) {
                 MPI_COMM_WORLD, &requests[req_count++]);
         }
 
-        // ---- OpenMP: ВЫЧИСЛЕНИЯ ----
-        // Внутренние точки (вдали от границ)
 #pragma omp parallel
         {
             double thread_diff = 0.0;
@@ -212,11 +195,8 @@ int main(int argc, char** argv) {
             {
                 if (thread_diff > local_diff) local_diff = thread_diff;
             }
-
-            // Ждем барьера OpenMP перед обработкой границ
 #pragma omp barrier
 
-// Точки рядом с нижней ghost границей
 #pragma omp for nowait
             for (int i = 1; i < Nx - 1; i++) {
                 for (int j = 1; j < Ny - 1; j++) {
@@ -234,7 +214,6 @@ int main(int argc, char** argv) {
                 }
             }
 
-            // Точки рядом с верхней ghost границей
 #pragma omp for
             for (int i = 1; i < Nx - 1; i++) {
                 for (int j = 1; j < Ny - 1; j++) {
@@ -258,7 +237,6 @@ int main(int argc, char** argv) {
             }
         }
 
-        // ---- MPI: ЖДЕМ ЗАВЕРШЕНИЯ ОБМЕНОВ ----
         if (req_count > 0) {
             MPI_Status statuses[4];
             MPI_Waitall(req_count, requests, statuses);
@@ -283,7 +261,6 @@ int main(int argc, char** argv) {
             }
         }
 
-        // Обновляем все значения
 #pragma omp parallel for
         for (int i = 1; i < Nx - 1; i++) {
             for (int j = 1; j < Ny - 1; j++) {
@@ -294,7 +271,6 @@ int main(int argc, char** argv) {
             }
         }
 
-        // Находим максимальное изменение
         MPI_Allreduce(&local_diff, &global_diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 
         iter++;
@@ -307,7 +283,6 @@ int main(int argc, char** argv) {
     double compute_time = MPI_Wtime() - compute_start;
     double total_time = compute_time + init_time;
 
-    // ========== ВЫЧИСЛЕНИЕ ОШИБКИ ==========
     double local_max_error = 0.0;
 
 #pragma omp parallel
@@ -345,7 +320,6 @@ int main(int argc, char** argv) {
     double global_max_error;
     MPI_Reduce(&local_max_error, &global_max_error, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    // ========== ВЫВОД РЕЗУЛЬТАТОВ ==========
     double max_total_time, max_compute_time;
     MPI_Reduce(&total_time, &max_total_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
     MPI_Reduce(&compute_time, &max_compute_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
@@ -367,7 +341,6 @@ int main(int argc, char** argv) {
         std::cout << "Converged: " << (global_diff <= EPS ? "YES" : "NO") << std::endl;
         std::cout << "========================================\n" << std::endl;
 
-        // Сохраняем в файл
         std::ofstream out("hybrid_results.txt", std::ios::app);
         if (out) {
             out << mpi_size << " " << omp_threads << " "
@@ -378,17 +351,16 @@ int main(int argc, char** argv) {
     }
     if (mpi_rank == 0) {
         std::cout << "\n=== MPI Stat ===" << std::endl;
-        // Можно вызвать MPI_Get_processor_name для информации
         char processor_name[MPI_MAX_PROCESSOR_NAME];
         int name_len;
         MPI_Get_processor_name(processor_name, &name_len);
         std::cout << "Processor: " << processor_name << std::endl;
     }
 
-    // Сбор статистики по коммуникациям
     if (mpi_prof_enabled) {
-        MPI_Pcontrol(0); // Выключить и вывести статистику
+        MPI_Pcontrol(0);
     }
     MPI_Finalize();
     return 0;
+
 }
